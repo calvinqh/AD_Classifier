07797da6166a49e56f0b7ece0386701ff4e72234
calvinqh <calvinquach@gmail.com>
2018-05-25 10:18:41 -0400

    Create run routine with util file to clean data and generate a SVM model.

--- src/run.py
+++ src/run.py
@@ -1,2 +1,30 @@
+from pyspark import SparkContext
+
+from pyspark.mllib.classification import SVMWithSGD, SVMModel
+
+from .utils import loadRosmapData
+
 if __name__ == "__main__":
     print("Executing main.")
+
+    sc = SparkContext(appName="AlzheimersDiseaseClassifier")
+
+    print("Loading Data from ROSMAP file..")
+    # Load data into the spark context
+    fileName = "../data/ROSMAP_RNASeq_entrez.csv"
+    data = loadRosmapData(sc,fileName)
+    print("Loading data complete!")
+    
+    # Build the model
+    model = SVMWithSGD.train(data, iterations=100)
+
+    # Evaluating the model on training data
+    
+
+    # Save and load the model
+    outputLocation = "./models/"
+    outputModel = "svmADClassifier.model"
+    outputFile = outputLocation+outputModel
+    model.save(sc, outputFile)
+
+    sampleModel = SVMModel.load(sc, outputFile)
---
+++ src/utils.py
@@ -0,0 +1,42 @@
+from pyspark import SparkContext
+
+from pyspark.mllib.regression import LabeledPoint
+
+def cleanSample(sample):
+    #print(type(sample[1]))
+    #Returns a labeled point with label and data
+    #Assumes label is the first column and data entries are the rest.
+    return LabeledPoint(getLabelID(sample[1]), sample[2:]) 
+
+def rosmapFilter(sample, *args):
+    for arg in args:
+        if sample[1] == arg:
+            return None 
+    return sample 
+
+def getLabelID(label):
+    if(label == '1'):
+        return 0
+    return 1
+
+def loadRosmapData(context, fileName):
+    # Read datafile into spark context
+    rawData = context.textFile(fileName)
+    
+    # Retrieve header of file
+    header = rawData.first()
+
+    # Remove the header from file
+    headerlessRawData = rawData.filter(lambda line: line != header)
+    
+    # Parse the line to remove the commas
+    allSamples = headerlessRawData.map(lambda line: line.split(','))
+
+    # Apply filter to remove samples with diagnosis of NA, 2,3,6
+    trainingSamples = allSamples.filter(lambda sample: rosmapFilter(sample, 'NA', '2', '3', '6'))
+
+    # Transform training samples to trainable data format
+    # Currently they are all strings
+    trainingSamples = trainingSamples.map(cleanSample)
+
+    return trainingSamples
